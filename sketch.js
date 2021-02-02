e1b7bb5f857aad8b954edd9ddf5737be935d1510
Ujjwal-6 <73328128+Ujjwal-6@users.noreply.github.com>
2021-02-02 16:03:36 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+var canvas , backgroundImg , database , form , player , game ;
+var gameState = 0;
+var playerCount ;
+
+function setup(){
+  createCanvas(400,400);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+
+}
+
+function draw(){
+
+}
